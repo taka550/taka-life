c88783a10274fc991b276b541047b449603c6076
taka550 <fb7gnkw4bb@privaterelay.appleid.com>
2026-07-26 21:30:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='taka-life-v2.8.4';
-const ASSETS=['./','./index.html','./styles.css?v=2.8.4','./app.js?v=2.8.4','./manifest.webmanifest?v=2.8.4','./mio-angel.webp?v=2.8.4','./mio-devil.webp?v=2.8.4','./mio-devil-v220.webp?v=2.8.4','./mio-angel-v220.webp?v=2.8.4','./icon.svg','./icon-192-v2.png','./icon-512-v2.png','./apple-touch-icon-v2.png'];
+const CACHE='taka-life-v2.8.5';
+const ASSETS=['./','./index.html','./styles.css?v=2.8.5','./app.js?v=2.8.5','./manifest.webmanifest?v=2.8.5','./mio-angel.webp?v=2.8.5','./mio-devil.webp?v=2.8.5','./mio-devil-v220.webp?v=2.8.5','./mio-angel-v220.webp?v=2.8.5','./icon.svg','./icon-192-v2.png','./icon-512-v2.png','./apple-touch-icon-v2.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
